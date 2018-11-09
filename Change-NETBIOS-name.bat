c3a77b4429563d5e63df6dfd1800f2b332e8159c
Sebastian Petersen <sebastian@srmail.dk>
2018-11-09 23:32:30 +0100

    Documentation.

--- Change-NETBIOS-name.bat
+++ Change-NETBIOS-name.bat
@@ -1,5 +1,11 @@
 @echo off
+
+:: Gets user input.
 set /p name="Enter PC-name: "
+
+:: Variables to shorten 'runas' command line.
 set "command=runas /user:%COMPUTERNAME%\MANIT wmic computersystem where caption='%COMPUTERNAME%' rename %name%"
 set "msg=echo Please reboot your PC for the chagne to take effect!"
+
+:: Runs the change NETBIOS name command as administrator.
 runas /user:%COMPUTERNAME%\MANIT "cmd /C %command% & %msg% & pause"
