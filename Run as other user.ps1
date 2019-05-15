1cff915f6aa099b390a49db377ddfa8460c1493f
Sebastian Petersen <sebkirller@gmail.com>
2019-05-15 23:43:35 +0200

    Add Run as other user.ps1
    Add file for running programs as other user. This still triggers UAC prompts. If imitated user is administrator then UAC prompt will not ask for password, only (Yes or No).
    This allows users to install software without giving admin privileges and still having full control.
    If Execution Policy is enabled the user can run the file with the following command: powershell.exe -ExecutionPolicy Bypass -File C:\Path\To\Script\Script.ps1

---
+++ Run as other user.ps1
@@ -0,0 +1,8 @@
+﻿# Sebastian Petersen
+# sebastian@srmail.dk
+
+$username = ""
+$password = ""
+$credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
+
+Start-Process powershell.exe -Credential ($credentials) -ArgumentList "-Command Start-Process -Wait -filepath 'C:\Path\To\File\ChromeStandaloneSetup64.exe'"
