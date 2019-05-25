fb9957afcfb66635bdd795494032d8833974da0d
Sebastian Petersen <sebkirller@gmail.com>
2019-05-25 21:48:14 +0200

    Refactor

--- Run as other user.ps1
+++ Run as other user.ps1
@@ -1,8 +1,23 @@
 ﻿# Sebastian Petersen
 # sebastian@srmail.dk
+ 
 
 $username = ""
 $password = ""
-$credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
 
-Start-Process powershell.exe -Credential ($credentials) -ArgumentList "-Command Start-Process -Wait -filepath 'C:\Path\To\File\ChromeStandaloneSetup64.exe'"
+
+$credentials = New-Object System.Management.Automation.PSCredential -ArgumentList `
+               @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
+
+
+# Audio alerts can be added to the command string with the following line
+# [console]::beep(900,300); [console]::beep(900,300); [console]::beep(900,300);
+
+$commandString = "-Command `
+                  Write-Host -ForegroundColor Yellow -BackgroundColor Black '`
+                  Attention! Citrix is downloading, do NOT close this window!!! `
+                  The window will close automatically when the install process is done.'; `
+                  Start-Process -Wait -filepath '\\192.168.204.28\sym_update\Citrixupd\CitrixReceiver.exe'"
+
+
+Start-Process powershell.exe -Credential ($credentials) -ArgumentList $commandString
