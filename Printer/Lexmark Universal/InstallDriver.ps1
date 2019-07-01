f74ca0191aa49b47250b2de9671a8bebb03dbf1b
Sebastian Rønnov Petersen <seba8305@edu.easj.dk>
2019-07-02 00:04:28 +0200

    Nicer if

--- Printer/Lexmark Universal/InstallDriver.ps1
+++ Printer/Lexmark Universal/InstallDriver.ps1
@@ -1,5 +1,7 @@
 # Elevate script
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+  Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit
+}
 
 # Install driver
 Write-Host "Installing Lexmark Universal v2 XL driver..."
