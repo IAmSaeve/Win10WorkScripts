cf5c96455bd3e6d5f38cb3f285a77df18dbe8bd6
Sebastian Rønnov Petersen <seba8305@edu.easj.dk>
2019-07-02 17:37:38 +0200

    Update Network drives.ps1
    Remove redundant calls to MainMenu

--- Network drives.ps1
+++ Network drives.ps1
@@ -21,35 +21,29 @@ Function MainMenu {
             1 {
                 New-PSDrive -Name "Trucksales" -Root "\\192.168.204.28\Trucksales" -Persist -PSProvider "FileSystem"
                 Start-Sleep -Seconds 2
-                MainMenu
             }
             2 {
                 New-PSDrive -Name "servicedk" -Root "\\192.168.204.28\servicedk" -Persist -PSProvider "FileSystem"
                 Start-Sleep -Seconds 2
-                MainMenu
             }
             3 {
                 New-PSDrive -Name "produkt" -Root "\\192.168.204.28\produkt" -Persist -PSProvider "FileSystem"
                 Start-Sleep -Seconds 2
-                MainMenu
             }
             4 {
                 # TODO: Check if this is correct server address
                 New-PSDrive -Name "Trucksales" -Root "\\mandknt\Trucksales" -Persist -PSProvider "FileSystem"
                 Start-Sleep -Seconds 2
-                MainMenu
             }
             5 {
                 New-PSDrive -Name "Fælles" -Root "\\192.168.204.28\Fælles" -Persist -PSProvider "FileSystem"
                 Start-Sleep -Seconds 2
-                MainMenu
             }
             6 {
                 Clear-Host
                 Write-Host "You have to manually add this drive."
                 Read-Host "Press any key to open network folder..."
                 Invoke-Item "\\192.168.204.28\"
-                MainMenu
             }
             Q {
                 Exit
